24d11b525ebc180193999b3ee01d3e6551196449
tugascrown204 <gajdertevet@hotmail.com>
2025-08-16 18:54:30 +0330

    feat: initial commit for src/utils.js

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+// Utility functions for refactoring tools
+
+// Example of a utility function that could be expanded later
+function analyzeCodeQuality(code) {
+    // Placeholder for analyzing code quality
+    return {};
+}
+
+module.exports = { analyzeCodeQuality };
